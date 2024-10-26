195fab4a39612b2a5fe7363c36c0040a2c558a6a
lzj1769 <lzj1769@gmail.com>
2024-10-26 02:08:24 +0000

    add plot function

--- src/cell2net/plotting/_prediction.py
+++ src/cell2net/plotting/_prediction.py
@@ -6,25 +6,29 @@ from cell2net.prediction.model import Cell2Net
 
 
 def train_history(
-    model: Cell2Net, figsize: tuple[float, float] | None = (4, 4), show: bool = True
+    model: Cell2Net, figsize: tuple[float, float] | None = (10, 4), show: bool = True
 ):
     # check if model is trained
     assert model.is_trained, print("Please first train the model!")
 
-    _, ax = plt.subplots(figsize=figsize)
+    _, (ax1, ax2) = plt.subplots(1, 2, figsize=figsize)
 
     # Plot train_loss and valid_loss against epoches
-    ax.plot(model.history["epochs"], model.history["train_loss"], label="Train", marker="o")  # type: ignore
-    ax.plot(model.history["epochs"], model.history["valid_loss"], label="Validation", marker="x")  # type: ignore
+    ax1.plot(model.history["epochs"], model.history["train_loss"], label="Train", marker="o")  # type: ignore
+    ax1.plot(model.history["epochs"], model.history["valid_loss"], label="Validation", marker="x")  # type: ignore
+    ax2.plot(model.history["epochs"], model.history["train_corr"], label="Train", marker="o")  # type: ignore
+    ax2.plot(model.history["epochs"], model.history["valid_corr"], label="Validation", marker="x")  # type: ignore
 
     # Add labels and title
-    ax.set_xlabel("Epochs")
-    ax.set_ylabel("Loss")
-    ax.set_title("Train vs Validation Loss")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.set_title("Train vs Validation")
 
-    ax.legend()
+    ax2.set_xlabel("Epochs")
+    ax2.set_ylabel("Correlation")
+    ax2.set_title("Train vs Validation")
 
-    if show:
-        plt.show()
-    else:
-        return ax
+    ax1.legend()
+    ax2.legend()
+
+    plt.show()

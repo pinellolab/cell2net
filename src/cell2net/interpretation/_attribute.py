ebfbef979484776e062a7af67f35b49d788a7303
lzj1769 <lzj1769@gmail.com>
2024-10-29 13:04:18 +0000

    update attribution computation

--- src/cell2net/interpretation/_attribute.py
+++ src/cell2net/interpretation/_attribute.py
@@ -97,6 +97,8 @@ def compute_peak_attribution(
     idx: list[int] | list[str] | None = None,
     batch_size: int = 8,
     num_workers: int = 1,
+    baseline: str = "max_dist",
+    n_steps: int = 50,
 ) -> np.ndarray:
     r"""
     Calculate the attribution of peak accessibility to gene expression.
@@ -106,11 +108,16 @@ def compute_peak_attribution(
     model : Cell2Net
         Model that has been trained
     idx : list[int] | list[str] | None, optional
-        A list of int or string to indicate, by default None
+        A list of int or string to indicate.
+        If set to None, use all cells. Default: None
     batch_size : int, optional
         Batch size, by default 32
     num_workers : int, optional
         Number of CPUs for dataloader, by default 4
+    baseline: str, optional
+        How to create baseline to compute integrated gradients. Default: "max_dist"
+    n_steps: int, optional
+        Number of steps used by the approximation method. Default: 50.
 
     Returns
     -------
@@ -143,42 +150,137 @@ def compute_peak_attribution(
     )
 
     logger.info("Compute attribution for peak accessibility")
-    peak_acc_attr = []
+    attr = []
     for data in tqdm(data_loader):
         peak_seq = data["peak_seq"].to(model.device).requires_grad_()
         peak_acc = data["peak_acc"].to(model.device).requires_grad_()
         peak_dist = data["peak_dist"].to(model.device).requires_grad_()
         tf_exp = data["tf_exp"].to(model.device).requires_grad_()
-        covariates = data["covariates"].to(model.device).requires_grad_()
-
-        _max_peak_acc = torch.from_numpy(
-            np.repeat(max_peak_acc[np.newaxis, :], peak_acc.shape[0], axis=0)
-        ).to(model.device)
+        covariates = data["covariates"].to(model.device)
 
         # Create baseline peak accessibility
-        # For each cell, if the target input peak has a number > 0, then the baseline will be 0
-        # Otherwise, the baseline will be the highest value of accessibility of this peak across all cells
-        _peak_acc = torch.where(
-            peak_acc > 0,
+        match baseline:
+            case "max_dist":
+                # For each cell, if the target input peak has a number > 0, then the baseline will be 0
+                # Otherwise, the baseline will be the highest value of accessibility of this peak across all cells
+                _max_peak_acc = torch.from_numpy(
+                    np.repeat(max_peak_acc[np.newaxis, :], peak_acc.shape[0], axis=0)
+                ).to(model.device)
+
+                _peak_acc = torch.where(
+                    peak_acc > 0,
+                    torch.zeros_like(peak_acc),
+                    _max_peak_acc,
+                )
+            case "zero":
+                _peak_acc = torch.zeros_like(peak_acc)
+            case "gaussian":
+                _peak_acc = peak_acc + torch.randn(peak_acc.size()) * 0.1
+
+        attributions = ig.attribute(
+            inputs=(peak_seq, peak_acc, peak_dist, tf_exp),
+            baselines=(peak_seq, _peak_acc, peak_dist, tf_exp),
+            additional_forward_args=covariates,
+            return_convergence_delta=False,
+            n_steps=n_steps,
+        )
+
+        attr.append(attributions[1].detach().cpu())
+
+        # Release GPU memory by moving other attributions to cpu
+        del attributions
+
+    attr = torch.cat(attr, dim=0).numpy()
+
+    return attr
+
+
+def compute_tf_attribution(
+    model: Cell2Net,
+    idx: list[int] | list[str] | None = None,
+    batch_size: int = 8,
+    num_workers: int = 1,
+    n_steps: int = 100,
+) -> np.ndarray:
+    r"""
+    Calculate the attribution of TF expression to target gene expression.
+
+    Parameters
+    ----------
+    model : Cell2Net
+        Model that has been trained
+    idx : list[int] | list[str] | None, optional
+        A list of int or string to indicate, by default None
+    batch_size : int, optional
+        Batch size, by default 32
+    num_workers : int, optional
+        Number of CPUs for dataloader, by default 4
+
+    Returns
+    -------
+    np.ndarray
+        An numpy array of TF attribution with a shape of (n_cells, n_tfs)
+    """
+    # create a dataloader
+    logger.info("Create dataloader")
+    data_loader = get_dataloader(
+        mdata=model.mdata,
+        covariates=model.covariates,
+        idx=idx,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=False,
+        shuffle=False,
+        drop_last=False,
+        persistent_workers=False,
+    )
+
+    model.module.train()
+
+    # Use Integrated Gradients to estimate feature importances
+    ig = IntegratedGradients(model.module)
+
+    # For each peak, find the highest value across all cells
+    # This value will be used as baseline for peaks that are not accessible
+    max_tf_exp = (
+        model.mdata["rna"].obsm["tf"].max(axis=0).toarray().flatten()  # type: ignore
+    )
+
+    logger.info("Compute attribution for TF expression")
+    attr = []
+    for data in tqdm(data_loader):
+        peak_seq = data["peak_seq"].to(model.device).requires_grad_()
+        peak_acc = data["peak_acc"].to(model.device).requires_grad_()
+        peak_dist = data["peak_dist"].to(model.device).requires_grad_()
+        tf_exp = data["tf_exp"].to(model.device).requires_grad_()
+        covariates = data["covariates"].to(model.device)
+
+        _max_tf_exp = torch.from_numpy(
+            np.repeat(max_tf_exp[np.newaxis, :], tf_exp.shape[0], axis=0)
+        ).to(model.device)
+
+        # Create baseline TF expression
+        # For each cell, if the target input > 0, then the baseline will be 0
+        # Otherwise, the baseline will be the highest value of across all cells
+        _tf_exp = torch.where(
+            tf_exp > 0,
             torch.zeros_like(peak_acc),
-            _max_peak_acc,
+            _max_tf_exp,
         )
 
-        attributions, _delta = ig.attribute(
-            inputs=(peak_seq, peak_acc, peak_dist, tf_exp, covariates),
-            baselines=(peak_seq, _peak_acc, peak_dist, tf_exp, covariates),
-            return_convergence_delta=True,
-            n_steps=50,
+        attributions = ig.attribute(
+            inputs=(peak_seq, peak_acc, peak_dist, tf_exp),
+            baselines=(peak_seq, peak_acc, peak_dist, _tf_exp),
+            additional_forward_args=covariates,
+            return_convergence_delta=False,
+            n_steps=n_steps,
         )
 
-        peak_acc_attr.append(attributions[1].detach().cpu())
+        attr.append(attributions[3].detach().cpu())
 
-        # move other attributions to cpu
-        attributions[0].detach().cpu()
-        attributions[2].detach().cpu()
-        attributions[3].detach().cpu()
+        # Release GPU memory
+        del attributions
 
-    peak_acc_attr = torch.cat(peak_acc_attr, dim=0).numpy()
-    # delta = torch.cat(delta, dim=0).numpy()
+    attr = torch.cat(attr, dim=0).numpy()
 
-    return peak_acc_attr
+    return attr
--- src/cell2net/interpretation/_peak_to_gene.py
+++ src/cell2net/interpretation/_peak_to_gene.py
@@ -1,3 +1,4 @@
+import anndata as ad
 import numpy as np
 import pandas as pd
 from mudata import MuData
@@ -10,6 +11,8 @@ def peak_to_gene(
     mdata: MuData,
     peak_acc_attr: np.ndarray,
     groupby: str,
+    reference: str = "rest",
+    method: str = "t-test",
 ) -> pd.DataFrame:
     """
     Extracts peak-to-gene links based on the attribution of peak accessibility
@@ -34,6 +37,69 @@ def peak_to_gene(
     assert groupby in mdata.obs.columns, print(f"Cannot find {groupby} in mdata.obs")
 
     groups = mdata.obs[groupby].values
+
+    assert len(groups) == peak_acc_attr.shape[0], print(
+        f"Length of grouby {len(groups)} is different from number of cells {peak_acc_attr.shape[0]}"
+    )
+
+    logger.info(f"Extract peak-to-gene links for {groupby}")
+    unique_groups, group_indices = np.unique(groups, return_inverse=True)  # type: ignore
+
+    df_list = []
+    # For each group, subset the attribution and perform t-test
+    for i, unique_group in enumerate(unique_groups):
+        _peak_acc_attr = peak_acc_attr[group_indices == i]
+        res = stats.ttest_1samp(
+            _peak_acc_attr, popmean=0, axis=0, alternative="two-sided"
+        )
+
+        df = pd.DataFrame(
+            data={"t-statistic": res[0], "pvalue": res[1], "cell_type": unique_group}
+        )
+        df["peak"] = mdata.uns["peak_to_gene"]["peak"]
+        df["gene"] = mdata.uns["peak_to_gene"]["gene"]
+
+        df_list.append(df)
+
+    df = pd.concat(df_list, axis=0).reset_index(drop=True)
+    df = df[["gene", "peak", "cell_type", "t-statistic", "pvalue"]]
+
+    return df
+
+
+def peak_to_gene_v2(
+    mdata: MuData,
+    peak_acc_attr: np.ndarray,
+    groupby: str,
+    reference: str = "rest",
+    method: str = "t-test",
+) -> pd.DataFrame:
+    """
+    Extracts peak-to-gene links based on the attribution of peak accessibility
+
+    This is done by performing t-test to compare the attributions to zero.
+    If the groupby is None, all cells are used, otherwise the test is done for each group.
+
+    Parameters
+    ----------
+    mdata : MuData
+        MuData object including RNA and ATAC modalities
+    peak_acc_attr : np.ndarray
+        A numpy array of peak accessibility attribution
+    groupby : str | None, optional
+        Name of one column in mdata.obs to group cells. by default None
+
+    Returns
+    -------
+    pd.DataFrame
+        Calculated t-statistic and p-value for each peak-to-gene link
+    """
+    assert groupby in mdata.obs.columns, print(f"Cannot find {groupby} in mdata.obs")
+
+    groups = mdata.obs[groupby].values
+
+    adata = ad.AnnData(X=peak_acc_attr)
+    adata.obs = mdata.obs.copy()
 
     assert len(groups) == peak_acc_attr.shape[0], print(
         f"Length of grouby {len(groups)} is different from number of cells {peak_acc_attr.shape[0]}"

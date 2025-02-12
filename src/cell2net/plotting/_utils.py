cb313fa0ec9fb93a174a0fb09d96cffca4a0a2de
lzj1769 <lzj1769@gmail.comz>
2025-02-12 17:19:03 +0800

    update plotting functions

--- src/cell2net/plotting/_utils.py
+++ src/cell2net/plotting/_utils.py
@@ -1,12 +1,5 @@
 from __future__ import annotations
 
-from collections.abc import Sequence
-
-import anndata as ad
-import scanpy as sc
-from adpbulk import ADPBulk
-
-_VarNames = str | Sequence[str]
 
 def check_if_adjustText():
     """
@@ -70,31 +63,3 @@ def check_if_logomaker():
             "logomaker is not installed. Please install it with: pip install logomaker"
         ) from None
     return lm
-
-def create_bulk_adata(adata: ad.AnnData, groupby: str) -> ad.AnnData:
-    """
-    Create
-
-    Parameters
-    ----------
-    adata : ad.AnnData
-        _description_
-    groupby : str
-        _description_
-
-    Returns
-    -------
-    ad.AnnData
-        _description_
-    """
-    adpb = ADPBulk(adata, [groupby])
-
-    # perform the pseudobulking
-    counts = adpb.fit_transform()
-
-    sample_meta = adpb.get_meta().set_index("SampleName")
-    adata_bulk = ad.AnnData(X=counts, obs=sample_meta)
-    adata_bulk.layers["counts"] = adata_bulk.X  # type: ignore
-    sc.pp.normalize_total(adata_bulk, target_sum=1e6, layer="counts")
-
-    return adata_bulk

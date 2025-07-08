ad8f0c254d4c4551446ca32fbf72a6fd47f4bfbc
lzj1769 <lzj1769@gmail.com>
2025-07-08 01:37:48 +0000

    rename file

--- src/cell2net/interpretation/__init__.py
+++ src/cell2net/interpretation/__init__.py
@@ -1,3 +1,3 @@
-from ._peak_to_gene import peak_attr, peak_to_gene
-from ._seq_to_gene import seq_attr
-from ._tf_to_gene import get_top_tfs, tf_attr, tf_to_gene
+from ._peak import peak_attr, peak_to_gene
+from ._sequence import seq_attr
+from ._tf import get_top_tfs, tf_attr, tf_to_gene

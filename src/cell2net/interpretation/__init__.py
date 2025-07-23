a86ae0f1659cf9943664507e60be9dbd7e882600
lzj1769 <lzj1769@gmail.com>
2025-07-23 20:17:54 +0000

    add function for sequence

--- src/cell2net/interpretation/__init__.py
+++ src/cell2net/interpretation/__init__.py
@@ -1,3 +1,3 @@
 from ._peak import peak_attr, peak_to_gene
-from ._sequence import seq_attr, saturation_mutagenesis
+from ._sequence import saturation_mutagenesis, deep_lift
 from ._tf import get_top_tfs, tf_attr, tf_to_gene
